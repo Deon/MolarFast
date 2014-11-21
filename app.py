0e943fba14683079dd8099a430f4b0ba4c632868
DeonHua <deonhbci@gmail.com>
2014-11-21 18:47:47 -0500

    Added files

---
+++ app.py
@@ -0,0 +1,12 @@
+__author__ = 'Owner'
+from flask import *
+import os
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "hello world!"
+
+if __name__ == ("__main__"):
+    app.run(debug=True,host='127.0.0.1',port=int(os.environ.get("PORT", 5000)))
