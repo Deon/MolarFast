da9542440f708af171a29854a3e9e11670544b88
DeonHua <deonhbci@gmail.com>
2014-11-21 18:48:57 -0500

    Added files

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "hello world!"
+    return render_template("index.html")
 
 if __name__ == ("__main__"):
     app.run(debug=True,host='127.0.0.1',port=int(os.environ.get("PORT", 5000)))
